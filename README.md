f4dc9c1a9dfdb00bb558cd7c029b4e3bf5a95b09
di3go-sona <diego.sonaglia@gmail.com>
2023-01-02 18:42:12 +0100

    fixed structure to make it more reproducible

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-### Mindmap Graph
+# Mindmap Graph
 Mindmap graph is a simple utiliy to visualize relations within your thoughts !
+
+## Usage
+It is a simple cli tool, in order to run it you can clone the repository and move inside, it 
+
+```
+git clone https://github.com/di3go-sona/mindmap-graph.git
+cd mindmap-graph
+pip install -r requirements.txt
+```
+
+Then you can run the command with the presets
+```
+python3 main.py
+```
+Or have a look at the options with 
+```
+python3 main.py --help
+```
--- main.py
+++ main.py
@@ -1,69 +1,61 @@
 
-# %%
+#%%
 import wikipedia
-
-
-
-
-# %%
-user_entities = [
-    "Artificial intelligence",
-    "Cryptography",
-    "Cybersecurity",
-    "Linux",
-    "C (Programming Language)",
-    "Machine Learning",
-    "Python",
-    "Mathematics",
-    "Statistics",
-    "Programming Language"
-    
-    ]
-
-corpus = {}
-for e in user_entities:
-    
-    
-    page_names = wikipedia.search(e)
-    for page_name in page_names:
-        try:
-            page = wikipedia.page(page_name)
-            print(f"###{page}")
-            corpus[page_name.title()] = page.links
-            break
-        except Exception as error:  
-            pass
-corpus
-# %%
-edges = []
-for page, entities in corpus.items():
-    print(f"### {page}")
-    for target_entity in entities:
-        if target_entity.lower() in [ k.lower() for k in corpus.keys()] or True:
-            print(page, '->', target_entity)
-            edges.append((page.lower(), target_entity.lower()))
-
-
-# %%
-from pyvis.network import Network
 import networkx
+import math
+import click
+from pyvis.network import Network
 
-nx = networkx.Graph()
+@click.command()
+@click.option('--input_path', default='data/topics.txt', help='The path from where topics list (one per row) should be sourced')
+@click.option('--output_path', default='data/topics.html', help='The path where to write the output')
+def cli(input_path, output_path):
 
 
+    with open(input_path, 'r') as fin:
+        user_entities = fin.readlines()
+        user_entities = [u.strip() for u in user_entities]
 
-for e in edges:
-    nx.add_edge(*e)
 
-to_remove = [n for n in nx if len(nx[n]) < 4]
-nx.remove_nodes_from(to_remove)
-for n in nx:
-    nx.nodes[n]['mass'] = len(nx[n]) * 5
-    nx.nodes[n]['size'] = len(nx[n]) * 5
+    corpus = {}
+    for e in user_entities:
+        page_names = wikipedia.search(e)
+        for page_name in page_names:
+            try:
+                page = wikipedia.page(page_name)
+                print(f"Searching '{e}' on wikipedia, matched '{page_name.title()}'")
+                corpus[page_name.title()] = page.links
+                break
+            except Exception as error:  
+                print(f"No result founds for keyword '{e}'")
 
-net = Network("1000px", "1000px")
-net.from_nx(nx)
-net.show('nx.html')
+
+    edges = []
+    for page, entities in corpus.items():
+        print(f"Downloading page '{page}' from wikipedia")
+        for target_entity in entities:
+            if target_entity.lower() in [ k.lower() for k in corpus.keys()] or True:
+                # print(page, '->', target_entity)
+                edges.append((page.lower(), target_entity.lower()))
+
+    nx = networkx.Graph()
+    for e in edges:
+        nx.add_edge(*e)
+
+    to_remove = [n for n in nx if len(nx[n]) < 4]
+    nx.remove_nodes_from(to_remove)
+    for n in nx:
+        nx.nodes[n]['mass'] = math.sqrt(len(nx[n]))
+        nx.nodes[n]['size'] = math.sqrt(len(nx[n]))
+
+    net = Network("1000px", "1000px")
+    net.from_nx(nx)
+    net.show_buttons()
+    net.show(output_path)
+
+
+cli()
+
 
 
 # %%
